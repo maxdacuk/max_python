570d3c0031f60eff7af259bb7e90277bbe5e0465
maxdacuk <maxdacuk123@gmail.com>
2024-04-02 17:03:20 +0300

    my first commit

---
+++ max_substr_count.py
@@ -0,0 +1,13 @@
+def max_substr_count(aString, needle):
+    max_count = 0
+    needle_len = len(needle)
+    
+    for i in range(len(aString)):
+        if aString[i:i+needle_len] == needle:
+            max_count += 1
+            
+    return max_count
+
+# myStr = 'Good Morning!'
+myStr = 'aaaaaaaaaa'
+print(max_substr_count(myStr, 'aa'))
