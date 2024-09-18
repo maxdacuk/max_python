bcab8d1754ce10c2ca0f6bb52021f6e51cec3336
Andrii Honchar <andrew@pdf-xchange.com>
2024-09-18 11:34:12 +0300

    Test cases for ireplace function

--- cron/period_verifier.py
+++ cron/period_verifier.py
@@ -7,4 +7,20 @@ def verify_cron_period(period_spec: str) -> bool:
     if not verify_minute(periods_parts[0]):
         return False
 
+    chunk = '5,8,9'
+    amin = 0
+    amax = 23
+    comma_chunks = chunk.split(',')
+    if all(c.isdigit() and amin <= int(c) <= amax for c in comma_chunks):
+        return True
+
+
+    for c in comma_chunks:
+        if not (c.isdigit() and amin <= int(c) <= amax):
+            return False
     return True
+
+
+
+    return True
+
--- max_string.py
+++ max_string.py
@@ -1,4 +1,4 @@
-class max_string:
+class CMaxString:
     def __init__(self, s: str):
         self.s = s
         
@@ -15,6 +15,23 @@ class max_string:
         return replaced
     
     
-max_str = max_string("<body text=%BODY>")
+max_str = CMaxString("<body text=%BODY>")
 replaced_max_str = max_str.ireplace("%body%", "black")
 print(replaced_max_str)
+
+# Testing
+test_case_seq_number = 1
+for test_case in [
+    {'string': "<body text=%BODY>", 'search': '%body', 'replace_with': 'black', 'expected': '<body text=black>'},
+    {'string': "Hello, Max", 'search': 'max', 'replace_with': 'Serg', 'expected': 'Hello, Serg'},
+    {'string': "A match I hope I can find", 'search': 'i ', 'replace_with': 'we ', 'expected': 'A match we hope we can find'},
+]:
+    aMaxStringInst = CMaxString(test_case['string'])
+    if test_case['expected'] == aMaxStringInst.ireplace(test_case['search'], test_case['replace_with']):
+        print('Test case {0} passed'.format(test_case_seq_number))
+    else:
+        print('Test case {0} failed: CMaxString({1}).ireplace("{2}", "{3}") != "{4}"'.format(
+            test_case_seq_number, test_case['string'], test_case['search'], test_case['replace_with'], test_case['expected'])
+        )
+    test_case_seq_number += 1
+
