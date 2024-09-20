90374cd73893ebed2b41d34617424f44ca1c8bc4
maxdacuk <165047803+maxdacuk@users.noreply.github.com>
2024-09-20 16:39:19 +0300

    Update max_string.py

--- max_string.py
+++ max_string.py
@@ -1,22 +1,21 @@
-class CMaxString:
+class max_string2:
     def __init__(self, s: str):
         self.s = s
         
     def ireplace(self, search: str, replace: str, count=0):
         replaced = ''
         i = 0
-        while i < len(replace):
-            if replace[i:i+len(search)].lower() == search.lower():
-                replaced += replace_with
+        while i < len(self.s):
+            if self.s[i:i+len(search)].lower() == search.lower(): # Since the data under the "search" variable is never changed, the .lower() here is a huge overhead!
+                replaced += replace
                 i += len(search)
             else:
-                replaced += replace[i]
+                replaced += self.s[i]
                 i += 1
         return replaced
     
-    
-max_str = CMaxString("<body text=%BODY>")
-replaced_max_str = max_str.ireplace("%body%", "black")
+max_str = max_string2("<body text=%BODY>")
+replaced_max_str = max_str.ireplace("%BODY%", "black")
 print(replaced_max_str)
 
 # Testing
@@ -26,7 +25,7 @@ for test_case in [
     {'string': "Hello, Max", 'search': 'max', 'replace_with': 'Serg', 'expected': 'Hello, Serg'},
     {'string': "A match I hope I can find", 'search': 'i ', 'replace_with': 'we ', 'expected': 'A match we hope we can find'},
 ]:
-    aMaxStringInst = CMaxString(test_case['string'])
+    aMaxStringInst = max_string2(test_case['string'])
     if test_case['expected'] == aMaxStringInst.ireplace(test_case['search'], test_case['replace_with']):
         print('Test case {0} passed'.format(test_case_seq_number))
     else:
@@ -34,4 +33,3 @@ for test_case in [
             test_case_seq_number, test_case['string'], test_case['search'], test_case['replace_with'], test_case['expected'])
         )
     test_case_seq_number += 1
-
