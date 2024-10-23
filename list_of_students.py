d0a1b27f8b7d0066ea1106e0600e391411b6cdba
maxdacuk <165047803+maxdacuk@users.noreply.github.com>
2024-10-23 15:57:10 +0300

    Add files via upload

---
+++ list_of_students.py
@@ -0,0 +1,30 @@
+import xml.etree.ElementTree as ET
+
+# Створення кореневого елемента
+root = ET.Element("students")
+
+# Список студентів
+students = [
+    {"first_name": "Maxym", "last_name": "Diachuk", "age": "15"},
+    {"first_name": "Pavlo", "last_name": "Stadnyk", "age": "15"},
+    {"first_name": "Arsen", "last_name": "Olih", "age": "16"}
+]
+
+# Додавання студентів до XML
+for student in students:
+    student_elem = ET.SubElement(root, "student")
+    
+    first_name_elem = ET.SubElement(student_elem, "first_name")
+    first_name_elem.text = student["first_name"]
+    
+    last_name_elem = ET.SubElement(student_elem, "last_name")
+    last_name_elem.text = student["last_name"]
+    
+    age_elem = ET.SubElement(student_elem, "age")
+    age_elem.text = student["age"]
+
+# Створення дерева та збереження в XML файл
+tree = ET.ElementTree(root)
+tree.write("D:\\Python\\students.xml", encoding="utf-8", xml_declaration=True)
+
+print("XML-файл створено!")
